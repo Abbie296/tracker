96cb8f201b4183c0b56b478ba9960d40226e1c05
Rodrigo Picazo Cobaleda <24804702@stu.mmu.ac.uk>
2025-01-23 17:24:47 +0000

    Added feature where if the database doesn't exist it will create one with all the tables

--- backend/index.cjs
+++ backend/index.cjs
@@ -1,18 +1,32 @@
 var express = require('express');
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('test.db');
+const db = new sqlite3.Database('database.db');
 
 db.serialize(() =>{
-    db.run('CREATE TABLE lorem(info TEXT)');
-    const stmt = db.prepare('INSERT INTO lorem VALUES (?)')
-    for  (let i = 0; i <10; i++){
-        stmt.run(`Ipsum${i}`);
-    }
-    stmt.finalize();
+    
+    db.run('CREATE TABLE "users" ("userId"	INTEGER NOT NULL UNIQUE,"password"	TEXT NOT NULL,"email"	TEXT NOT NULL UNIQUE,"age"	INTEGER NOT NULL,"sex"	TEXT NOT NULL,"height"	NUMERIC NOT NULL,"weight"	NUMERIC NOT NULL,"phonenumber"	TEXT NOT NULL UNIQUE,"name "	TEXT NOT NULL,PRIMARY KEY("userId" AUTOINCREMENT));',(err)=> {
+        if (err){
+            console.log("hi")
+        }
+    });
 
-    db.each('SELECT rowid AS id, info FROM lorem',(err,row) => {
-        console.log(`${row.id}: ${row.info}`);
-    })
+
+    db.run('CREATE TABLE "foods" ("foodId"	INTEGER NOT NULL UNIQUE,"foodName"	TEXT NOT NULL,"ingredient"	TEXT NOT NULL,"rating"	INTEGER NOT NULL,"date"	TEXT NOT NULL,PRIMARY KEY("foodId" AUTOINCREMENT));',(err)=> {
+        if (err){
+            console.log("hi2")
+        }
+    });
+
+
+    // const stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+    // for  (let i = 0; i <10; i++){
+    //     stmt.run(`Ipsum${i}`);
+    // }
+    // stmt.finalize();
+
+    // db.each('SELECT rowid AS id, info FROM lorem',(err,row) => {
+    //     console.log(`${row.id}: ${row.info}`);
+    // })
 })
 const app = express();
 var sql = ('mssql');
