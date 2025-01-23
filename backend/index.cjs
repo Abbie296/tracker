a49a77714ad9ee384dc21fc7d19032f54b9a767b
Rodrigo Picazo Cobaleda <24804702@stu.mmu.ac.uk>
2025-01-23 19:30:20 +0000

    Added functionality to be able to display all info from the users table

--- backend/index.cjs
+++ backend/index.cjs
@@ -1,36 +1,26 @@
+// Documentation
+// https://github.com/TryGhost/node-sqlite3/wiki/API
+
+
 var express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('database.db');
+const app = express();
+var port = '3000';
 
+// Creates database and tables if the database is gone
 db.serialize(() =>{
-    
     db.run('CREATE TABLE "users" ("userId"	INTEGER NOT NULL UNIQUE,"password"	TEXT NOT NULL,"email"	TEXT NOT NULL UNIQUE,"age"	INTEGER NOT NULL,"sex"	TEXT NOT NULL,"height"	NUMERIC NOT NULL,"weight"	NUMERIC NOT NULL,"phonenumber"	TEXT NOT NULL UNIQUE,"name "	TEXT NOT NULL,PRIMARY KEY("userId" AUTOINCREMENT));',(err)=> {
         if (err){
-            console.log("hi")
+            console.log("'users' table already exists")
         }
     });
-
-
     db.run('CREATE TABLE "foods" ("foodId"	INTEGER NOT NULL UNIQUE,"foodName"	TEXT NOT NULL,"ingredient"	TEXT NOT NULL,"rating"	INTEGER NOT NULL,"date"	TEXT NOT NULL,PRIMARY KEY("foodId" AUTOINCREMENT));',(err)=> {
         if (err){
-            console.log("hi2")
+            console.log("'foods' table already exists")
         }
     });
-
-
-    // const stmt = db.prepare('INSERT INTO lorem VALUES (?)');
-    // for  (let i = 0; i <10; i++){
-    //     stmt.run(`Ipsum${i}`);
-    // }
-    // stmt.finalize();
-
-    // db.each('SELECT rowid AS id, info FROM lorem',(err,row) => {
-    //     console.log(`${row.id}: ${row.info}`);
-    // })
-})
-const app = express();
-var sql = ('mssql');
-var port = '3000';
+});
 
 
 
@@ -42,7 +32,9 @@ app.get('/',function(req,res){
 
 //Displays all the info from users
 app.get('/users', function(req,res){
-    
+    db.get(`SELECT * FROM users;`,(err,row) => {
+        res.send(row);
+    });
 });
 
 // Start the server
