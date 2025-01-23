a0bdc980e7f9379218034b8c5a1c49b4d444352c
Abbie296 <abbieroberts296@gmail.com>
2025-01-23 18:02:24 +0000

    added styles
    added fonts to html. styled text. styled home page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,9 @@
-const Signup = () => {
-    return <h1>Signup</h1>;
-  };
-  
-  export default Signup;
+import React from 'react'
+
+export default function Signup() {
+  return (
+    <div>
+      <h1>Signup</h1>
+    </div>
+  )
+}
